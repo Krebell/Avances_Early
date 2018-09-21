028eaa659de8cd34dbcc0e5700b2877727717bae
Dante <dante.lzrss@gmail.com>
2018-09-21 03:20:51 -0500

    Cambios

--- earlyw2v.py
+++ earlyw2v.py
@@ -299,8 +299,8 @@ for i in range(len(clas)):
 		erde5[i] = 1.0
 		erde50[i] = 1.0
 	elif(pclases[i] == 1 and clas[i] == 1):
-		erde5[i] = 1.0 - (1.0/(1.0+np.exp(finalpost[i]-5)))##Se necesita cambiar por el numero correcto de post del usuario, por mientras se deja el promedio
-		erde50[i] = 1.0 - (1.0/(1.0+np.exp(finalpost[i]-50)))##Se necesita cambiar por el numero correcto de post del usuario, por mientras se deja el promedio				
+		erde5[i] = 1.0 - (1.0/(1.0+np.exp(finalpost[i]-5)))##
+		erde50[i] = 1.0 - (1.0/(1.0+np.exp(finalpost[i]-50)))##
 	elif(pclases[i] == 0 and clas[i] == 0):
 		erde5[i] = 0.0
 		erde50[i] = 0.0
